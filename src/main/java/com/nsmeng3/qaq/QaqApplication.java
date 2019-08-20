c7441b1d2818c79b254ab6ec0ddf0e98c9cd8702
DifferentW <nsmeng3@163.com>
2019-08-20 11:05:52 +0800

    init repo

---
+++ src/main/java/com/nsmeng3/qaq/QaqApplication.java
@@ -0,0 +1,13 @@
+package com.nsmeng3.qaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nsmeng3/qaq/QaqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nsmeng3.qaq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QaqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
